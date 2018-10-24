4d8dd3337d70f7fac3ff0171ece9162f6f17911d
Victor Wade <44330039+vwade@users.noreply.github.com>
2018-10-23 20:54:48 -0700

    added ft_memcpy.c for libft

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: viwade <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/23 20:11:26 by viwade            #+#    #+#             */
-/*   Updated: 2018/10/23 20:54:02 by viwade           ###   ########.fr       */
+/*   Updated: 2018/10/23 20:54:48 by viwade           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	while (n-- > 0)
 	{
 		tmp = &dst[i];
-		tmp[0] = (unsigned char)src[i];
+		tmp[0] = (unsigned char *)src[i];
 		i++;
 	}
 	return (dst);
