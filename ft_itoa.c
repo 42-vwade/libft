ce1979b192a8db907c4d94fd13b13c1e35c5a615
Victor Wade <44330039+vwade@users.noreply.github.com>
2018-10-25 19:28:07 -0700

    added ft_itoa.c for libft

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: viwade <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/23 21:58:04 by viwade            #+#    #+#             */
-/*   Updated: 2018/10/25 16:33:43 by viwade           ###   ########.fr       */
+/*   Updated: 2018/10/25 19:27:36 by viwade           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,8 @@ char	*ft_itoa(int n)
 	while (tmp /= 10)
 		len++;
 	len += size;
-	if ((str = (char *)malloc(len * sizeof(*str))))
+	if ((str = (char *)malloc(len-- * sizeof(*str))))
 	{
-		str[--len] = 0;
 		while (len--)
 		{
 			tmp = (n < 0) ? -(n % 10) : n % 10;
