1f0cecb62e00890012d53c1e97a25944f3b93643
Victor Wade <44330039+vwade@users.noreply.github.com>
2018-10-26 21:14:14 -0700

    added ft_list.h for libft

---
+++ ft_list.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: viwade <viwade@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/24 20:07:34 by viwade            #+#    #+#             */
+/*   Updated: 2018/10/24 20:22:14 by viwade           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LIST_H
+# define FT_LIST_H
+
+typedef struct		s_list
+{
+	struct s_list	*next;
+	void			*data;
+}					t_list;
+
+t_list				*ft_create_elem(void *data);
+
+#endif
