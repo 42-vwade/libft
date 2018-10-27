231f4c9b6e6101081e16cc0475544bd38a2051d8
Victor Wade <44330039+vwade@users.noreply.github.com>
2018-10-26 21:40:38 -0700

    added libft.h for libft

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: viwade <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/23 16:34:03 by viwade            #+#    #+#             */
-/*   Updated: 2018/10/25 16:52:28 by viwade           ###   ########.fr       */
+/*   Updated: 2018/10/26 21:39:41 by viwade           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,30 @@
 # define LIBFT_H
 # include <stdlib.h>
 
-void	ft_putchar(char c);
-void	ft_putstr(char const *s);
-int		ft_atoi(const char *str);
-char	*ft_itoa(int n);
-void	*ft_memset(void *b, int c, size_t len);
+void		ft_putchar(char c);
+void		ft_putstr(char const *s);
+int			ft_atoi(const char *str);
+char		*ft_itoa(int n);
+void		*ft_memset(void *b, int c, size_t len);
+size_t		ft_strlen(const char s*);
+char		*ft_strdup(const char *s1);
+char		*ft_strcpy(char *dst, const char *src);
+char		*ft_strncpy(char *dst, const char *src, size_t len);
+char		*ft_strcat(char *s1, const char *s2);
+char		*ft_strncat(char *s1, const char *s2, size_t n);
+size_t		*ft_strlcat(char *dst, const char *src, size_t dstsize);
+char		*ft_strchr(const char *s, int c);
+char		*ft_strrchr(const char *s, int c);
+char		*ft_strstr(const char *haystack, const char *needle);
+char		*ft_strnstr(const char *haystack, const char *needle, size_t len);
+int			ft_strcmp(const char *s1, const char *s2);
+int			ft_strncmp(const char *s1, const char *s2, size_t n);
+int			ft_isalpha(int c);
+int			ft_isdigit(int c);
+int			ft_isalnum(int c);
+int			ft_isascii(int c);
+int			ft_isprint(int c);
+int			ft_toupper(int c);
+int			ft_tolower(int c);
 
 #endif
