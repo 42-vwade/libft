d5acea557fc5ce756529cb8904b9eac791a5817c
Victor Wade <44330039+vwade@users.noreply.github.com>
2018-10-28 18:50:39 -0700

    added ../../ft_memcpy.c for test

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: viwade <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/23 20:11:26 by viwade            #+#    #+#             */
-/*   Updated: 2018/10/28 17:52:51 by viwade           ###   ########.fr       */
+/*   Updated: 2018/10/28 18:21:15 by viwade           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	while (n-- > 0)
 	{
 		tmp = &dst[i];
-		tmp[0] = (unsigned char)src[i];
+		tmp[0] = ((unsigned char *)src)[i];
 		i++;
 	}
 	return (dst);
