72332fe8c66f56cdd3b8440be14bd241d4904344
Victor Wade <44330039+vwade@users.noreply.github.com>
2018-10-31 11:46:10 -0700

    added ./ft_atoi.c ./ft_bzero.c ./ft_create_elem.c ./ft_isalnum.c ./ft_isalpha.c ./ft_isascii.c ./ft_isdigit.c ./ft_isprint.c ./ft_itoa.c ./ft_list.h ./ft_memalloc.c ./ft_memccpy.c ./ft_memchr.c ./ft_memcmp.c ./ft_memcpy.c ./ft_memdel.c ./ft_memmove.c ./ft_memset.c ./ft_putchar.c ./ft_putendl.c ./ft_putnbr.c ./ft_putstr.c ./ft_putstr_fd.c ./ft_strcat.c ./ft_strchr.c ./ft_strclr.c ./ft_strcmp.c ./ft_strcpy.c ./ft_strdel.c ./ft_strdup.c ./ft_strequ.c ./ft_striter.c ./ft_striteri.c ./ft_strjoin.c ./ft_strlcat.c ./ft_strlen.c ./ft_strmap.c ./ft_strmapi.c ./ft_strncat.c ./ft_strncmp.c ./ft_strncpy.c ./ft_strnequ.c ./ft_strnew.c ./ft_strnstr.c ./ft_strrchr.c ./ft_strrev.c ./ft_strstr.c ./ft_tolower.c ./ft_toupper.c libft.h for libft

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: viwade <viwade@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/31 13:38:07 by viwade            #+#    #+#             */
-/*   Updated: 2018/10/31 11:28:53 by viwade           ###   ########.fr       */
+/*   Updated: 2018/10/31 11:46:03 by viwade           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,19 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	char	*cpy;
 	size_t	i;
 	size_t	len;
 
-	cpy = dst;
 	i = 0;
 	len = 0;
+	if (!dst || !src)
+		return (0);
 	while (dst[len])
 		++len;
-	if (dstsize > 0 && dstsize > len && *src)
-		while (i++ < dstsize - len - 1)
-			if ((dst[len + i - 1] = src[i - 1]))
-				;
-			else
-				dst[i] = '\0';
+	if (dstsize > 0 && dstsize > len)
+		while (src[i] && (i < dstsize - len - 1))
+			if ((dst[len + i] = src[i]))
+				i++;
+	dst[len + i] = '\0';
 	return (len + i);
 }
