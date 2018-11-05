d7bb5ee11c33d0d9b94bdd764734c57ff50d7a71
Victor Wade <44330039+vwade@users.noreply.github.com>
2018-11-05 03:03:45 -0800

    added ft_atoi.c for libft

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: viwade <viwade@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/30 10:54:25 by viwade            #+#    #+#             */
-/*   Updated: 2018/11/04 11:09:22 by viwade           ###   ########.fr       */
+/*   Updated: 2018/11/05 03:02:57 by viwade           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,8 @@ int		ft_atoi(const char *str)
 		return (num);
 	while ((*str > 0 && *str <= 32) || *str == 127)
 		str++;
-	neg = (str += (*str == '-' || *str == '+') ? 1 : 0)[0];
+	neg = str[0];
+	str += (*str == '-' || *str == '+') ? 1 : 0;
 	while (*str >= '0' && *str <= '9')
 		num = num * 10 + (*str++ - 48);
 	return ((neg == '-') ? -num : num);
