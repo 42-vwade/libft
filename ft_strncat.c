253361402cb67299a07cf2d0f5f089e846c9ceb8
Victor Wade <44330039+vwade@users.noreply.github.com>
2018-11-05 15:44:44 -0800

    added ft_atoi.c ft_bzero.c ft_create_elem.c ft_isalnum.c ft_isalpha.c ft_isascii.c ft_isdigit.c ft_isprint.c ft_itoa.c ft_lst2arr.c ft_lstadd.c ft_lstdel.c ft_lstdelone.c ft_lstiter.c ft_lstlen.c ft_lstmap.c ft_lstnew.c ft_memalloc.c ft_memccpy.c ft_memchr.c ft_memcmp.c ft_memcpy.c ft_memdel.c ft_memmove.c ft_memset.c ft_putchar.c ft_putchar_fd.c ft_putendl.c ft_putendl_fd.c ft_putnbr.c ft_putnbr_fd.c ft_putstr.c ft_putstr_fd.c ft_strcat.c ft_strchr.c ft_strclr.c ft_strcmp.c ft_strcpy.c ft_strdel.c ft_strdup.c ft_strequ.c ft_striter.c ft_striteri.c ft_strjoin.c ft_strlcat.c ft_strlen.c ft_strmap.c ft_strmapi.c ft_strncat.c ft_strncmp.c ft_strncpy.c ft_strnequ.c ft_strnew.c ft_strnstr.c ft_strrchr.c ft_strrev.c ft_strsplit.c ft_strstr.c ft_strsub.c ft_strtrim.c ft_tolower.c ft_toupper.c for libft

--- ft_strncat.c
+++ ft_strncat.c
@@ -6,26 +6,20 @@
 /*   By: viwade <viwade@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/31 03:57:32 by viwade            #+#    #+#             */
-/*   Updated: 2018/11/05 15:03:30 by viwade           ###   ########.fr       */
+/*   Updated: 2018/11/05 15:44:33 by viwade           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <string.h>
 
-char	*ft_strncat(char *dest, const char *src, size_t nb)
+char	*ft_strncat(char *s1, const char *s2, size_t n)
 {
-	size_t	i[2];
-	char	*cat;
+	size_t	i;
+	char	*ret;
 
-	if (!dest || !src)
-		return (dest);
-	i[0] = 0;
-	i[1] = 0;
-	cat = dest;
-	while (dest[i[0]])
-		i[0]++;
-	while ((dest[i[0] + i[1]] = src[i[1]]) && nb-- > (0))
-		dest[i[0] + i[1]++ + 1] = 0;
-	return (cat);
+	ret = s1;
+	s1 += ft_strlen(s1);
+	while ((s1[i] = s2[i]) && i < n)
+		s1[i++ + 1] = 0;
+	return (ret);
 }
