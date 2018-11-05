4b5b601351a487dfc42335aa83e3fc8569724621
Victor Wade <44330039+vwade@users.noreply.github.com>
2018-11-05 13:39:28 -0800

    added ft_strncat.c for libft

--- ft_strncat.c
+++ ft_strncat.c
@@ -6,7 +6,7 @@
 /*   By: viwade <viwade@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/31 03:57:32 by viwade            #+#    #+#             */
-/*   Updated: 2018/11/05 08:28:22 by viwade           ###   ########.fr       */
+/*   Updated: 2018/11/05 13:38:55 by viwade           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,16 @@
 char	*ft_strncat(char *dest, const char *src, size_t nb)
 {
 	size_t	i;
-	char	*cpy;
+	char	*cat;
 
 	i = 0;
 	if (!dest || !src)
 		return (dest);
-	cpy = dest;
+	cat = dest;
 	while (*dest)
 		dest++;
-	while (src[i++] && nb-- > 0)
-		dest[i - 1] = src[i - 1];
-	return (cpy);
+	while ((dest[i] = src[i]) && nb-- > (0))
+		if (nb)
+			dest[i++ + 1] = 0;
+	return (cat);
 }
