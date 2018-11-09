b2d575485f6c92761a1b4a1aac465acd56c06374
Victor Wade <44330039+vwade@users.noreply.github.com>
2018-11-09 10:39:36 -0800

    added ft_atoi.c ft_bzero.c ft_create_elem.c ft_isalnum.c ft_isalpha.c ft_isascii.c ft_isdigit.c ft_isprint.c ft_itoa.c ft_lst2arr.c ft_lstadd.c ft_lstdel.c ft_lstdelone.c ft_lstiter.c ft_lstlen.c ft_lstmap.c ft_lstnew.c ft_memalloc.c ft_memccpy.c ft_memchr.c ft_memcmp.c ft_memcpy.c ft_memdel.c ft_memmove.c ft_memset.c ft_putchar.c ft_putchar_fd.c ft_putendl.c ft_putendl_fd.c ft_putnbr.c ft_putnbr_fd.c ft_putstr.c ft_putstr_fd.c ft_strcat.c ft_strchr.c ft_strclr.c ft_strcmp.c ft_strcpy.c ft_strdel.c ft_strdup.c ft_strequ.c ft_striter.c ft_striteri.c ft_strjoin.c ft_strlcat.c ft_strlen.c ft_strmap.c ft_strmapi.c ft_strncat.c ft_strncmp.c ft_strncpy.c ft_strnequ.c ft_strnew.c ft_strnstr.c ft_strrchr.c ft_strrev.c ft_strsplit.c ft_strstr.c ft_strsub.c ft_strtrim.c ft_tolower.c ft_toupper.c libft.h for libft

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,11 +6,11 @@
 /*   By: viwade <viwade@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/31 02:20:13 by viwade            #+#    #+#             */
-/*   Updated: 2018/11/09 10:36:32 by viwade           ###   ########.fr       */
+/*   Updated: 2018/11/09 10:39:32 by viwade           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-static	int	valid_char(int c)
+static	int	isvalid(int c)
 {
 	if ((unsigned)c < 0x100 || c == -1)
 		return (1);
@@ -22,7 +22,7 @@ int			ft_isalpha(int c)
 	unsigned char a;
 
 	a = (unsigned char)c;
-	if (valid_char(c))
+	if (isvalid(c))
 		if ((a >= 'A' && a <= 'Z') || (a >= 'a' && a <= 'z'))
 			return (1);
 	return (0);
