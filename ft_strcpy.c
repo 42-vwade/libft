b57313e7ab59c306d92120cf4ba3aab761dfd028
Victor Wade <44330039+vwade@users.noreply.github.com>
2018-11-10 12:02:41 -0800

    added ft_atoi.c ft_bzero.c ft_create_elem.c ft_die.c ft_isalnum.c ft_isalpha.c ft_isascii.c ft_isdigit.c ft_isprint.c ft_itoa.c ft_lst2arr.c ft_lstadd.c ft_lstdel.c ft_lstdelone.c ft_lstiter.c ft_lstlen.c ft_lstmap.c ft_lstnew.c ft_memalloc.c ft_memccpy.c ft_memchr.c ft_memcmp.c ft_memcpy.c ft_memdel.c ft_memmove.c ft_memset.c ft_putchar.c ft_putchar_fd.c ft_putendl.c ft_putendl_fd.c ft_putnbr.c ft_putnbr_fd.c ft_putstr.c ft_putstr_fd.c ft_strcat.c ft_strchr.c ft_strclr.c ft_strcmp.c ft_strcpy.c ft_strdel.c ft_strdup.c ft_strequ.c ft_striter.c ft_striteri.c ft_strjoin.c ft_strlcat.c ft_strlen.c ft_strmap.c ft_strmapi.c ft_strncat.c ft_strncmp.c ft_strncpy.c ft_strnequ.c ft_strnew.c ft_strnstr.c ft_strrchr.c ft_strrev.c ft_strsplit.c ft_strstr.c ft_strsub.c ft_strtrim.c ft_tolower.c ft_toupper.c libft.h for libft

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -6,7 +6,7 @@
 /*   By: viwade <viwade@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/30 15:01:08 by viwade            #+#    #+#             */
-/*   Updated: 2018/11/10 10:59:27 by viwade           ###   ########.fr       */
+/*   Updated: 2018/11/10 11:38:35 by viwade           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*ft_strcpy(char *dest, const char *src)
 
 	cpy = dest;
 	if (!dest || !src)
-		ft_die("KILL(ft_strcpy): Cannot copy. Parameters invalid.");
+		ft_die("FT_STRCPY: Died.");
 	while ((*dest++ = *src++))
 		;
 	return (cpy);
--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: viwade <viwade@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/30 15:46:08 by viwade            #+#    #+#             */
-/*   Updated: 2018/11/10 11:25:13 by viwade           ###   ########.fr       */
+/*   Updated: 2018/11/10 12:02:28 by viwade           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,13 @@
 char
 	*ft_strncpy(char *dst, const char *src, size_t n)
 {
-	char *cpy;
+	size_t	len;
 
-	cpy = dst;
 	if (!dst || !src)
-		ft_die("FT_STRCAT: Cannot concatenate. Parameters invalid.");
-	while (n--)
-		if (!*src)
-			while (*dst)
-				*dst++ = '\0';
-		else
-			*dst++ = *src++;
-	return (cpy);
+		ft_die("FT_STRNCAT: Died.");
+	ft_memmove(dst, src, n);
+	if (n < ft_strlen(src))
+		while (dst[n])
+			dst[n++] = 0;
+	return (dst);
 }
