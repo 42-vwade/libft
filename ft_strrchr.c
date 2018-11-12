d5aa546e0515cd8ec37a4b71b0f3be8621052098
Victor Wade <44330039+vwade@users.noreply.github.com>
2018-11-11 23:56:56 -0800

    added ft_atoi.c ft_bzero.c ft_create_elem.c ft_die.c ft_isalnum.c ft_isalpha.c ft_isascii.c ft_isdigit.c ft_isprint.c ft_itoa.c ft_lst2arr.c ft_lstadd.c ft_lstdel.c ft_lstdelone.c ft_lstiter.c ft_lstlen.c ft_lstmap.c ft_lstnew.c ft_memalloc.c ft_memccpy.c ft_memchr.c ft_memcmp.c ft_memcpy.c ft_memdel.c ft_memmove.c ft_memset.c ft_putchar.c ft_putchar_fd.c ft_putendl.c ft_putendl_fd.c ft_putnbr.c ft_putnbr_fd.c ft_putstr.c ft_putstr_fd.c ft_strcat.c ft_strchr.c ft_strclr.c ft_strcmp.c ft_strcpy.c ft_strdel.c ft_strdup.c ft_strequ.c ft_striter.c ft_striteri.c ft_strjoin.c ft_strlcat.c ft_strlen.c ft_strmap.c ft_strmapi.c ft_strncat.c ft_strncmp.c ft_strncpy.c ft_strnequ.c ft_strnew.c ft_strnstr.c ft_strrchr.c ft_strrev.c ft_strsplit.c ft_strstr.c ft_strsub.c ft_strtrim.c ft_tolower.c ft_toupper.c libft.h for libft

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: viwade <viwade@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/31 03:44:09 by viwade            #+#    #+#             */
-/*   Updated: 2018/11/02 04:08:58 by viwade           ###   ########.fr       */
+/*   Updated: 2018/11/11 23:56:43 by viwade           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,15 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	size_t			i;
-	unsigned char	*p;
+	unsigned long long int	len;
 
-	i = ft_strlen(s);
 	if (!s)
-		return (NULL);
-	while (i-- > 0)
+		ft_die("");
+	len = ft_strlen(&s[0]);
+	while (len--)
 	{
-		p = &((unsigned char *)s)[i];
-		if ((unsigned char)c == p[0])
-			return ((char *)p);
+		if (s[len + 1] == (unsigned char)c)
+			return ((char *)&s[len + 1]);
 	}
-	return (NULL);
+	return ((s[len] == (unsigned char)c) ? (char *)&s[len] : NULL);
 }
