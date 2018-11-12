464a17e874e4ad3d29b7e0f68cc079a9cf7d174c
Victor Wade <44330039+vwade@users.noreply.github.com>
2018-11-11 21:34:24 -0800

    added ft_atoi.c ft_bzero.c ft_create_elem.c ft_die.c ft_isalnum.c ft_isalpha.c ft_isascii.c ft_isdigit.c ft_isprint.c ft_itoa.c ft_lst2arr.c ft_lstadd.c ft_lstdel.c ft_lstdelone.c ft_lstiter.c ft_lstlen.c ft_lstmap.c ft_lstnew.c ft_memalloc.c ft_memccpy.c ft_memchr.c ft_memcmp.c ft_memcpy.c ft_memdel.c ft_memmove.c ft_memset.c ft_putchar.c ft_putchar_fd.c ft_putendl.c ft_putendl_fd.c ft_putnbr.c ft_putnbr_fd.c ft_putstr.c ft_putstr_fd.c ft_strcat.c ft_strchr.c ft_strclr.c ft_strcmp.c ft_strcpy.c ft_strdel.c ft_strdup.c ft_strequ.c ft_striter.c ft_striteri.c ft_strjoin.c ft_strlcat.c ft_strlen.c ft_strmap.c ft_strmapi.c ft_strncat.c ft_strncmp.c ft_strncpy.c ft_strnequ.c ft_strnew.c ft_strnstr.c ft_strrchr.c ft_strrev.c ft_strsplit.c ft_strstr.c ft_strsub.c ft_strtrim.c ft_tolower.c ft_toupper.c libft.h for libft

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,24 +6,23 @@
 /*   By: viwade <viwade@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/30 20:10:20 by viwade            #+#    #+#             */
-/*   Updated: 2018/11/11 21:22:24 by viwade           ###   ########.fr       */
+/*   Updated: 2018/11/11 21:32:42 by viwade           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 char
-	*ft_strnstr(const char *s, const char *c, size_t n)
+	*ft_strnstr(const char *s, const char *c, size_t slen)
 {
-	size_t	i;
+	unsigned long long int		i;
+	size_t						len;
 
 	i = 0;
-	if (!s || !c)
-		ft_die("");
-	while (s[i] && n)
-		if (n--)
-			if (s[i++] == c[0])
-				if (!ft_strncmp(&s[i - 1], c, n))
-					return ((char *)&s[i - 1]);
-	return ((c[0]) ? NULL : (char *)s);
+	len = ft_strlen(&c[0]);
+	while (*s && len && slen-- > len - 1)
+		if (*s++ == *c)
+			if (!ft_strncmp(s, &c[1], len - 1))
+				return ((char *)--s);
+	return ((len > slen || c[0]) ? NULL : (char *)s);
 }
