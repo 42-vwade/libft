6909651e8694f19ca0064ed823b3d72d6909583d
Victor Wade <44330039+vwade@users.noreply.github.com>
2018-11-13 14:03:58 -0800

    added ft_astrcmp.c ft_atoi.c ft_bzero.c ft_create_elem.c ft_die.c ft_isalnum.c ft_isalpha.c ft_isascii.c ft_isdigit.c ft_isprint.c ft_itoa.c ft_lst2arr.c ft_lstadd.c ft_lstdel.c ft_lstdelone.c ft_lstiter.c ft_lstlen.c ft_lstmap.c ft_lstnew.c ft_memalloc.c ft_memccpy.c ft_memchr.c ft_memcmp.c ft_memcpy.c ft_memdel.c ft_memmove.c ft_memset.c ft_putchar.c ft_putchar_fd.c ft_putendl.c ft_putendl_fd.c ft_putnbr.c ft_putnbr_fd.c ft_putstr.c ft_putstr_fd.c ft_strcat.c ft_strchr.c ft_strclr.c ft_strcmp.c ft_strcpy.c ft_strdel.c ft_strdup.c ft_strequ.c ft_striter.c ft_striteri.c ft_strjoin.c ft_strlcat.c ft_strlen.c ft_strmap.c ft_strmapi.c ft_strncat.c ft_strncmp.c ft_strncpy.c ft_strnequ.c ft_strnew.c ft_strnstr.c ft_strrchr.c ft_strrev.c ft_strsplit.c ft_strstr.c ft_strsub.c ft_strtrim.c ft_tolower.c ft_toupper.c libft.h for libft

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,16 +6,20 @@
 /*   By: viwade <viwade@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/30 23:49:16 by viwade            #+#    #+#             */
-/*   Updated: 2018/11/13 13:20:26 by viwade           ###   ########.fr       */
+/*   Updated: 2018/11/13 14:03:50 by viwade           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string.h>
+
 int
-	ft_strncmp(char *s1, char *s2, unsigned int n)
+	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	int		i;
 
 	i = 0;
+	if (!n)
+		return (0);
 	while (n--)
 		if (s1[i] != s2[i])
 			break ;
@@ -25,5 +29,5 @@ int
 			break ;
 		else
 			i++;
-	return ((unsigned char)(s1[i] - s2[i]));
-}
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
+	}
