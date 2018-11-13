ee36a061c75977d5cf2edc3c6b6e33afa7511e5d
Victor Wade <44330039+vwade@users.noreply.github.com>
2018-11-12 16:25:29 -0800

    added ft_atoi.c ft_bzero.c ft_create_elem.c ft_die.c ft_isalnum.c ft_isalpha.c ft_isascii.c ft_isdigit.c ft_isprint.c ft_itoa.c ft_lst2arr.c ft_lstadd.c ft_lstdel.c ft_lstdelone.c ft_lstiter.c ft_lstlen.c ft_lstmap.c ft_lstnew.c ft_memalloc.c ft_memccpy.c ft_memchr.c ft_memcmp.c ft_memcpy.c ft_memdel.c ft_memmove.c ft_memset.c ft_putchar.c ft_putchar_fd.c ft_putendl.c ft_putendl_fd.c ft_putnbr.c ft_putnbr_fd.c ft_putstr.c ft_putstr_fd.c ft_strcat.c ft_strchr.c ft_strclr.c ft_strcmp.c ft_strcpy.c ft_strdel.c ft_strdup.c ft_strequ.c ft_striter.c ft_striteri.c ft_strjoin.c ft_strlcat.c ft_strlen.c ft_strmap.c ft_strmapi.c ft_strncat.c ft_strncmp.c ft_strncpy.c ft_strnequ.c ft_strnew.c ft_strnstr.c ft_strrchr.c ft_strrev.c ft_strsplit.c ft_strstr.c ft_strsub.c ft_strtrim.c ft_tolower.c ft_toupper.c libft.h for libft

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: viwade <viwade@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/29 20:46:53 by viwade            #+#    #+#             */
-/*   Updated: 2018/11/12 16:22:45 by viwade           ###   ########.fr       */
+/*   Updated: 2018/11/12 16:25:24 by viwade           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,6 @@
 int
 	ft_strnequ(char const *s1, char const *s2, size_t n)
 {
-	if (!n)
-		return (1);
 	if (!s1 || !s2)
 		return (0);
 	while (n--)
@@ -25,6 +23,6 @@ int
 		else if (!*++s1 && !*++s2)
 			return (1);
 		else if (!*s1 || !*s2)
-			break ;
-	return ((s1[0] == s2[0]) ? 1 : 0);
+			return (0);
+	return (1);
 }
