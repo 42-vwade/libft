d7fc4aae4922de4358f17c2071ed309c5d935e71
Victor Wade <44330039+vwade@users.noreply.github.com>
2018-11-15 00:34:03 -0800

    added ft_arrlen.c ft_astrcmp.c ft_atoi.c ft_bzero.c ft_create_elem.c ft_die.c ft_isalnum.c ft_isalpha.c ft_isascii.c ft_isdigit.c ft_isprint.c ft_itoa.c ft_lst2arr.c ft_lstadd.c ft_lstdel.c ft_lstdelone.c ft_lstiter.c ft_lstlen.c ft_lstmap.c ft_lstnew.c ft_memalloc.c ft_memccpy.c ft_memchr.c ft_memcmp.c ft_memcpy.c ft_memdel.c ft_memmove.c ft_memset.c ft_putchar.c ft_putchar_fd.c ft_putendl.c ft_putendl_fd.c ft_putnbr.c ft_putnbr_fd.c ft_putstr.c ft_putstr_fd.c ft_strcat.c ft_strchr.c ft_strclr.c ft_strcmp.c ft_strcpy.c ft_strdel.c ft_strdup.c ft_strequ.c ft_striter.c ft_striteri.c ft_strjoin.c ft_strlcat.c ft_strlen.c ft_strmap.c ft_strmapi.c ft_strncat.c ft_strncmp.c ft_strncpy.c ft_strnequ.c ft_strnew.c ft_strnstr.c ft_strrchr.c ft_strrev.c ft_strsplit.c ft_strstr.c ft_strsub.c ft_strtrim.c ft_tolower.c ft_toupper.c libft.h for libft

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: viwade <viwade@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/03 04:28:50 by viwade            #+#    #+#             */
-/*   Updated: 2018/11/03 04:36:30 by viwade           ###   ########.fr       */
+/*   Updated: 2018/11/15 00:33:59 by viwade           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,11 @@ t_list	*ft_lstnew(void const *content, size_t content_size)
 	t_list		*new;
 
 	if ((new = (t_list *)malloc(sizeof(*new))))
-	{
-		new->content = (content) ? (void *)content : NULL;
-		new->content_size = (content) ? content_size : 0;
-		new->next = NULL;
-	}
+		if ((new->content = ft_memalloc(content_size)))
+		{
+			new->content = ft_memcpy(new->content, content, content_size);
+			new->content_size = (content) ? content_size : 0;
+			new->next = NULL;
+		}
 	return (new);
 }
