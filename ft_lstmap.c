6b1ca2ebbd8ad8287a8767b38d25f8586bd02231
Victor Wade <44330039+vwade@users.noreply.github.com>
2018-11-16 13:28:53 -0800

    added ft_arrlen.c ft_astrcmp.c ft_atoi.c ft_bzero.c ft_create_elem.c ft_die.c ft_isalnum.c ft_isalpha.c ft_isascii.c ft_isdigit.c ft_isprint.c ft_itoa.c ft_lst2arr.c ft_lstadd.c ft_lstdel.c ft_lstdelone.c ft_lstiter.c ft_lstlen.c ft_lstmap.c ft_lstnew.c ft_memalloc.c ft_memccpy.c ft_memchr.c ft_memcmp.c ft_memcpy.c ft_memdel.c ft_memmove.c ft_memset.c ft_putchar.c ft_putchar_fd.c ft_putendl.c ft_putendl_fd.c ft_putnbr.c ft_putnbr_fd.c ft_putstr.c ft_putstr_fd.c ft_strcat.c ft_strchr.c ft_strclr.c ft_strcmp.c ft_strcpy.c ft_strdel.c ft_strdup.c ft_strequ.c ft_striter.c ft_striteri.c ft_strjoin.c ft_strlcat.c ft_strlen.c ft_strmap.c ft_strmapi.c ft_strncat.c ft_strncmp.c ft_strncpy.c ft_strnequ.c ft_strnew.c ft_strnstr.c ft_strrchr.c ft_strrev.c ft_strsplit.c ft_strstr.c ft_strsub.c ft_strtrim.c ft_tolower.c ft_toupper.c libft.h for libft

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: viwade <viwade@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/03 05:45:30 by viwade            #+#    #+#             */
-/*   Updated: 2018/11/04 08:45:51 by viwade           ###   ########.fr       */
+/*   Updated: 2018/11/16 13:28:50 by viwade           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,13 @@
 t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
 	t_list	*map;
-	t_list	*next;
 
-	if (!lst || !f)
-		return (NULL);
-	if ((map = f(ft_lstnew(lst->content, lst->content_size))))
-		while ((lst = lst->next))
+	if (lst && f)
+		if ((map = (t_list *)malloc(sizeof(*map))))
 		{
-			if (!(next = f(ft_lstnew(lst->content, lst->content_size))))
-				return (NULL);
-			map->next = next;
-			next = next->next;
+			map->content = f(lst->content);
+			map->next = ft_lstmap(lst->next, f);
+			return (map);
 		}
-	return (map);
+	return (NULL);
 }
