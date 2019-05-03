37ce830be69ba5e0e64811344d53e75e74095d0b
Victor Wade <viwade@student.42.us.org>
2019-05-03 01:06:55 -0700

    added . for libft

--- assets/added_functions/ft_printdouble.c
+++ assets/added_functions/ft_printdouble.c
@@ -6,26 +6,20 @@
 /*   By: viwade <viwade@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/30 07:18:13 by viwade            #+#    #+#             */
-/*   Updated: 2019/05/03 00:29:53 by viwade           ###   ########.fr       */
+/*   Updated: 2019/05/03 01:05:35 by viwade           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft_core/libft.h"
+#include "../libft_core/libft.h"
 #include <unistd.h>
 #define D_MASK (0xFFFFFFFF00000000u)
-
-static double
-	abs(double n)
-{
-	return (n < 0 ? -n : n);
-}
 
 static void
 	print_decimal(double n)
 {
 	while (n)
 	{
-		write(1, (long)(n * 10) % 10 + 48, 1);
+		write(1, (char[1]){(long)(n * 10) % 10 + 48}, 1);
 		n = n * 10 - (long)(n * 10);
 	}
 }
@@ -35,5 +29,5 @@ void
 {
 	ft_putnbr((long)n);
 	write(1, ".", 1);
-	print_decimal(abs(n - (long)n));
+	print_decimal(ABS(n - (long)n));
 }
