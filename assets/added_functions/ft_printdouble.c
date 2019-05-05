079b14b029b01ca0ffec0ab01bbadbcc698acd00
Victor Wade <viwade@student.42.us.org>
2019-05-05 12:20:34 -0700

    added . for libft

--- assets/added_functions/ft_printdouble.c
+++ assets/added_functions/ft_printdouble.c
@@ -6,7 +6,7 @@
 /*   By: viwade <viwade@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/30 07:18:13 by viwade            #+#    #+#             */
-/*   Updated: 2019/05/03 01:05:35 by viwade           ###   ########.fr       */
+/*   Updated: 2019/05/05 12:20:08 by viwade           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,40 @@
 #include <unistd.h>
 #define D_MASK (0xFFFFFFFF00000000u)
 
-static void
-	print_decimal(double n)
+static int
+	round_up(double n)
 {
-	while (n)
+	return ((int)((n - (int)n) * 10) >= 5);
+}
+
+static void
+	print_decimal(size_t d, double n, int p)
+{
+	size_t	num;
+	size_t	len;
+
+	num = 0;
+	while (n && p--)
 	{
-		write(1, (char[1]){(long)(n * 10) % 10 + 48}, 1);
+		num = num * 10 + (int)(n * 10);
 		n = n * 10 - (long)(n * 10);
+	}
+	len = ft_intlen(num);
+	num += !p && round_up(n * 10);
+	d += ft_intlen(num) > len;
+	ft_putnbr(d);
+	if (p)
+	{
+		num %= ft_power(10, len + 1);
+		write(1, ".", 1);
+		ft_putnbr(num);
 	}
 }
 
 void
-	ft_printdouble(double n)
+	ft_printdouble(double n, unsigned int precision)
 {
-	ft_putnbr((long)n);
-	write(1, ".", 1);
-	print_decimal(ABS(n - (long)n));
+	if (n < 0 && write(1, "-", 1))
+		n = ABS(n);
+	print_decimal((size_t)n, ABS(n - (long)n), precision);
 }
