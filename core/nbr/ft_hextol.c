4b794f1b3787a6b3ae2609d960560fb64008a230
Victor Wade <viwade@student.42.us.org>
2019-07-10 16:36:47 -0700

    added . for libft

--- core/nbr/ft_hextol.c
+++ core/nbr/ft_hextol.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_htol.c                                          :+:      :+:    :+:   */
+/*   ft_hextol.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: viwade <viwade@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/10 15:02:18 by viwade            #+#    #+#             */
-/*   Updated: 2019/07/10 15:37:09 by viwade           ###   ########.fr       */
+/*   Updated: 2019/07/10 16:35:20 by viwade           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,13 @@
 **	HEXADECIMAL TO LONG
 */
 
-char
+static char
 	hex_value(char c)
 {
 	if (48 <= c && c <= 57)
 		return (c - 48);
-	else if (65 <= c && c <= 70)
-		return (c - 65);
-	else if (97 <= c && c <= 102)
-		return (c - 97);
+	else if ((97 <= c && c <= 102) || (65 <= c && c <= 70))
+		return (c - 55 + 32 * (97 <= c && c <= 102));
 	else
 		return (-1);
 }
@@ -36,7 +34,7 @@ long long
 	num = 0;
 	while ((9 <= s[0] && s[0] <= 13) || s[0] == 32)
 		s++;
-	while ((val = hex_value(s[0])) > -1 && hex_value(s[0]) < 16)
+	while ((val = hex_value(s[0])) > -1)
 		num = num * 16 + val;
 	return (num);
 }
