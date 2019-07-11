2eebd7021049ac937cb465260bd3b8d2bd62b156
Victor Wade <viwade@student.42.us.org>
2019-07-11 15:17:04 -0700

    added . for libft

--- core/lst/ft_lstdel.c
+++ core/lst/ft_lstdel.c
@@ -6,12 +6,19 @@
 /*   By: viwade <viwade@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/03 04:58:47 by viwade            #+#    #+#             */
-/*   Updated: 2019/05/13 19:06:11 by viwade           ###   ########.fr       */
+/*   Updated: 2019/07/11 15:16:54 by viwade           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../libft.h"
 #include <stdlib.h>
+
+/*
+**	Traverses to end of list.
+**	Uses delete function on content of list node.
+**	Then deletes the node.
+**	Traverse to previous node, repeat.
+*/
 
 void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 {
--- core/nbr/ft_hextol.c
+++ core/nbr/ft_hextol.c
@@ -6,7 +6,7 @@
 /*   By: viwade <viwade@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/10 15:02:18 by viwade            #+#    #+#             */
-/*   Updated: 2019/07/10 18:34:20 by viwade           ###   ########.fr       */
+/*   Updated: 2019/07/10 23:17:16 by viwade           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,9 +36,24 @@ unsigned long long
 	num = 0;
 	while ((9 <= s[0] && s[0] <= 13) || s[0] == 32)
 		s++;
-	s += 2 * (s[0] == '0' && (s[0] && s[1] == 'x'));
+	s += 2 * ((s[0] == '0' || s[0] == '\\') && (s[0] && s[1] == 'x'));
 	s += (s[0] == '#');
 	while ((val = hex_value(s[0])) > -1)
 		num = num * 16 + val;
+	return (num);
+}
+
+unsigned long long
+	ft_octtol(char *s)
+{
+	unsigned long long	num;
+	char				val;
+
+	num = 0;
+	while ((9 <= s[0] && s[0] <= 13) || s[0] == 32)
+		s++;
+	s += (s[0] == '0' && (s[0] && ('1' <= s[1] && s[1] <= '8')));
+	while (('0' <= s[0] && s[0] <= '8'))
+		num = num * 8 + val;
 	return (num);
 }
