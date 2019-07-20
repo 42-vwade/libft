840ff0bbfc715d0556a2aa59351d314352676741
Victor Wade <viwade@student.42.us.org>
2019-07-20 07:57:55 -0700

    added . for libft

--- ext/get_next_line/get_next_line.h
+++ ext/get_next_line/get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: viwade <viwade@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/18 21:04:41 by viwade            #+#    #+#             */
-/*   Updated: 2019/05/13 18:45:09 by viwade           ###   ########.fr       */
+/*   Updated: 2019/07/20 07:46:24 by viwade           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # define FD_LIMIT 8192
-# define BUFF_SIZE 21
+# define BUFF_SIZE 1048576
 
 # include "../../libft.h"
 # include <sys/types.h>
