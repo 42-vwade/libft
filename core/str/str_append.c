31e35329a5658e9463c7a6cba4baf1274b23f284
Victor Wade <viwade@student.42.us.org>
2019-09-03 23:08:05 -0700

    added . for libft

--- core/str/str_append.c
+++ core/str/str_append.c
@@ -11,25 +11,29 @@
 /* ************************************************************************** */
 
 #include "../../libft.h"
-#define FREE_BOTH	0
+#define FREE_NONE	0
 #define FREE_LEFT	1
 #define FREE_RIGHT	2
-#define APPEND_0(s,a,b) {(s) = ft_strjoin(a,b);ft_memdel(&a);ft_memdel(&b);}
+#define FREE_ALL	3
+#define APPEND_0(s,a,b) {(s) = ft_strjoin(a,b);}
 #define APPEND_1(s,a,b) {(s) = ft_strjoin(a,b);ft_memdel(&a);}
 #define APPEND_2(s,a,b) {(s) = ft_strjoin(a,b);ft_memdel(&b);}
-#define MODE_0(s,x,a,b) if(x==FREE_BOTH)	APPEND_0(s,a,b)
+#define APPEND_3(s,a,b) {(s) = ft_strjoin(a,b);ft_memdel(&a);ft_memdel(&b);}
+#define MODE_0(s,x,a,b) if(x==FREE_NONE)	APPEND_0(s,a,b)
 #define MODE_1(s,x,a,b) if(x==FREE_LEFT)	APPEND_1(s,a,b)
 #define MODE_2(s,x,a,b) if(x==FREE_RIGHT)	APPEND_2(s,a,b)
+#define MODE_3(s,x,a,b) if(x==FREE_ALL)		APPEND_3(s,a,b)
 
 char	*ft_append(const char *s1, const char *s2, int mode)
 {
 	char	*string;
 
-	if (mode != FREE_BOTH || mode != FREE_LEFT || mode != FREE_RIGHT)
+	if ((unsigned)mode > FREE_ALL)
 		ft_error("ft_append: no mode specified");
 	string = 0;
 	MODE_0(string, mode, s1, s2);
 	MODE_1(string, mode, s1, s2);
 	MODE_2(string, mode, s1, s2);
+	MODE_3(string, mode, s1, s2);
 	return (string);
 }
