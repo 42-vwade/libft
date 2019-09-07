1cd23ddc3a36838fa7378a255c8b39f2807ec5bb
Victor Wade <viwade@student.42.us.org>
2019-09-07 14:37:45 -0700

    1928/2437 on Gavin's test.

--- core/nbr/ft_atoi.c
+++ core/nbr/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: viwade <viwade@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/30 10:54:25 by viwade            #+#    #+#             */
-/*   Updated: 2018/11/14 22:44:25 by viwade           ###   ########.fr       */
+/*   Updated: 2019/09/07 13:54:05 by viwade           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@ int		ft_atoi(const char *str)
 	long long int		num;
 	char				neg;
 
-	num = 0;
+	if ((num = !str))
+		return (0);
 	while ((*str >= 9 && *str <= 13) || (*str == 32))
 		str++;
 	neg = str[0];
